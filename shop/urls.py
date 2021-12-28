c5a38839e152e4ce42594f7bf2e22d30edbc329d
sling254 <mwanikidenis7@gmail.com>
2021-12-28 10:07:15 +0300

    add a javascript to add items to cart and an update view

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView,ProductsView
+from .views import IndexView,ProductsView,CheckoutView,UpdateView
 
 urlpatterns = [
     path('', IndexView, name='index'),
     path('products/', ProductsView, name='products'),
+    path('checkout/', CheckoutView, name='checkout'),
+    path('UpdateItem/', UpdateView, name='UpdateItem'),
    
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,4 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,3 +10,9 @@ def IndexView(request):
 
 def ProductsView(request):
     return render(request, 'products.html')
+
+def CheckoutView(request):
+    return render(request, 'checkout.html')
+
+def UpdateView(request):
+    return JsonResponse({'status': 'ok'}, safe=False)
