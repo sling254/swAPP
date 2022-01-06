a5a2c43958d71043b83818564d576aa55d21e12e
sling254 <32681973+sling254@users.noreply.github.com>
2022-01-06 12:32:45 +0300

    Update the database name

--- swAPP/settings.py
+++ swAPP/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'swAPP.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
