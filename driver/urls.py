7058d167130c6c1bab53f463d05a76dd7767cef2
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-11 11:20:01 +0300

    add single_driver function

--- driver/urls.py
+++ driver/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . views import  drivers, create_driver
+from . views import  drivers, create_driver,single_driver
 
 urlpatterns = [
     # path('drivers_dash/', drivers_dash, name='drivers_dash'),
     path("create_driver", create_driver, name="create_driver"), 
-    path('drivers/', drivers, name='drivers')
+    path('drivers/', drivers, name='drivers'),
+    path('drivers/<str:name>', single_driver, name='single_driver'),
    
    
 ]
