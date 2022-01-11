bcdc198621d7801c00490248058e34cbc34141f8
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2022-01-11 03:40:39 +0300

    modify project urls

--- swAPP/urls.py
+++ swAPP/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('shop.urls')),
-    path('Drivers/',include('driver.urls')),
+    path('drivers/',include('driver.urls')),
+    # path('Drivers/',include('driver.urls')),
 ]
